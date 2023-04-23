709c3752948c60461daa08650288a05f8ee7f5d3
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-04-23 12:13:07 +0000

    commiting printf project

--- _putchar.c
+++ _putchar.c
@@ -2,10 +2,11 @@
 
 /**
  * _putchar - writes a single character to the output.
+ * @c: character input
  * Return: returns a single character.
  */
 
 int _putchar(char c)
 {
-	return(write(1, &c, 1));
+	return (write(1, &c, 1));
 }
