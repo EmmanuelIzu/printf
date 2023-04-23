1120c63dba58fa9d1ddd90cc7b3cac81b9f3624c
Emmanuel Izuchukwu <emmanuelizu92@gmail.com>
2023-04-23 12:07:39 +0000

    commiting printf project

--- _printf.c
+++ _printf.c
@@ -1,8 +1,34 @@
 #include "main.h"
-
-int _printf(const char *format, ...)
+#include <stdarg.h>
+/**
+ * check_person - check for %
+ * @c: character to be checked
+ * Return: return i;
+ */
+int check_person(char c)
 {
 	int i = 0;
+
+	if (c == '%')
+		return (i = 1);
+return (i);
+}
+
+
+/**
+ * _printf - prints output to thr o
+ * @format: a variadic argument
+ * Return: Return an interger
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0;
+
+	if (format)
+	{
+
+	va_start(args, format);
 
 	while (format[i] != '\0')
 	{
@@ -10,6 +36,6 @@ int _printf(const char *format, ...)
 		i++;
 	}
 
-
-return(0);
+	}
+return (0);
 }
